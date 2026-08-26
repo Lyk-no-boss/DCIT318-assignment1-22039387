bd5435bf1ae095ceea57e82771fc81b836cbe8fc
Stephen Acquah <sacquah048@st.ug.edu.gh>
2026-08-26 16:19:00 +0000

    Add Grade Calculator

---
+++ GradeCalculator/GradeCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your grade (0-100): ");
+        double grade = Convert.ToDouble(Console.ReadLine());
+
+        if (grade < 0 || grade > 100)
+        {
+            Console.WriteLine("Invalid grade. Please enter a grade between 0 and 100.");
+        }
+        else if (grade >= 90)
+        {
+            Console.WriteLine("Your Letter Grade is: A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your Letter Grade is: B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your Letter Grade is: C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your Letter Grade is: D");
+        }
+        else
+        {
+            Console.WriteLine("Your Letter Grade is: F");
+        }
+    }
+}
