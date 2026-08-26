96eeb8a638c070e9f998a851c0281d0a2fb21c99
Stephen Acquah <sacquah048@st.ug.edu.gh>
2026-08-26 17:13:33 +0000

    Add Ticket Price Calculator

---
+++ TicketPriceCalculator/TicketPriceCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 0)
+        {
+            Console.WriteLine("Invalid age.");
+        }
+        else if (age <= 12 || age >= 65)
+        {
+            Console.WriteLine(" Your Ticket Price: GHC 7");
+        }
+        else
+        {
+            Console.WriteLine("Your Ticket Price: GHC 10");
+        }
+    }
+}
